5a34a4b0d9856a4ec408c79066559e235989ff01
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2019-11-16 21:48:47 +0100

    Add validation if email exists

--- app/src/main/java/com/example/dreamtunes/SignUpActivity.java
+++ app/src/main/java/com/example/dreamtunes/SignUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.dreamtunes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.dreamtunes.Services.UserService;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.EnumMap;
 import java.util.Map;
@@ -29,7 +35,9 @@ public class SignUpActivity extends AppCompatActivity {
     private Button facebook;
 
     private Map<Inputs, EditText> inputs = new EnumMap<>(Inputs.class);
+    private Map<Inputs, TextView> validators = new EnumMap<>(Inputs.class);
     private Map<Inputs, Boolean> validatorLock = new EnumMap<>(Inputs.class);
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
 
     @SuppressLint("ClickableViewAccessibility")
@@ -50,6 +58,11 @@ public class SignUpActivity extends AppCompatActivity {
         inputs.put(Inputs.SURNAME, (EditText) findViewById(R.id.input_surname));
         inputs.put(Inputs.EMAIL, (EditText) findViewById(R.id.input_email));
         inputs.put(Inputs.PASSWORD, (EditText) findViewById(R.id.input_password));
+
+        validators.put(Inputs.NAME, (TextView) findViewById(R.id.validator_name));
+        validators.put(Inputs.SURNAME, (TextView) findViewById(R.id.validator_surname));
+        validators.put(Inputs.EMAIL, (TextView) findViewById(R.id.validator_email));
+        validators.put(Inputs.PASSWORD, (TextView) findViewById(R.id.validator_password));
 
         validatorLock.put(Inputs.NAME, false);
         validatorLock.put(Inputs.SURNAME, false);
@@ -74,13 +87,11 @@ public class SignUpActivity extends AppCompatActivity {
         inputs.get(Inputs.NAME).addTextChangedListener(new InputValidator((EditText) findViewById(R.id.input_name)) {
             @Override
             public void validate(EditText textView, String text) {
-                TextView validator = findViewById(R.id.validator_name);
-
                 if (text.length() == 0) {
-                    validator.setText(R.string.name_required);
+                    validators.get(Inputs.NAME).setText(R.string.name_required);
                     validatorLock.put(Inputs.NAME, false);
                 } else {
-                    validator.setText(null);
+                    validators.get(Inputs.NAME).setText(null);
                     validatorLock.put(Inputs.NAME, true);
                 }
             }
@@ -89,13 +100,11 @@ public class SignUpActivity extends AppCompatActivity {
         inputs.get(Inputs.SURNAME).addTextChangedListener(new InputValidator((EditText) findViewById(R.id.input_surname)) {
             @Override
             public void validate(EditText textInput, String text) {
-                TextView validator = findViewById(R.id.validator_surname);
-
                 if (text.length() == 0) {
-                    validator.setText(R.string.surname_required);
+                    validators.get(Inputs.SURNAME).setText(R.string.surname_required);
                     validatorLock.put(Inputs.SURNAME, false);
                 } else {
-                    validator.setText(null);
+                    validators.get(Inputs.SURNAME).setText(null);
                     validatorLock.put(Inputs.SURNAME, true);
                 }
 
@@ -106,20 +115,21 @@ public class SignUpActivity extends AppCompatActivity {
         inputs.get(Inputs.EMAIL).addTextChangedListener(new InputValidator((EditText) findViewById(R.id.input_email)) {
             @Override
             public void validate(EditText textInput, String text) {
-                TextView validator = findViewById(R.id.validator_email);
-
                 if (text.length() == 0) {
-                    validator.setText(R.string.email_required);
+                    validators.get(Inputs.EMAIL).setText(R.string.email_required);
                     validatorLock.put(Inputs.EMAIL, false);
                 } else {
                     if (!text.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
-                        validator.setText(R.string.wrong_format);
+                        validators.get(Inputs.EMAIL).setText(R.string.wrong_format);
                         validatorLock.put(Inputs.EMAIL, false);
                     } else {
-                        validator.setText(null);
+
+                        validators.get(Inputs.EMAIL).setText(null);
                         validatorLock.put(Inputs.EMAIL, true);
+
                     }
                 }
+
 
             }
         });
@@ -128,7 +138,6 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void validate(EditText textInput, String text) {
                 TextView validator = findViewById(R.id.validator_password);
-
                 if (text.length() == 0) {
                     validator.setText(R.string.password_required);
                     validatorLock.put(Inputs.PASSWORD, false);
@@ -149,41 +158,43 @@ public class SignUpActivity extends AppCompatActivity {
         signUp.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN: {
                         v.setBackgroundResource(R.drawable.ic_sign_up_button_clicked);
                         break;
                     }
                     case MotionEvent.ACTION_UP: {
-
                         v.setBackgroundResource(R.drawable.ic_sign_up_button);
 
                         if (isValid()) {
-
-                            Intent userService = createUserServiceIntent();
-
-                            userService.putExtra("name", inputs.get(Inputs.NAME).getText().toString());
-                            userService.putExtra("surname", inputs.get(Inputs.SURNAME).getText().toString());
-                            userService.putExtra("email", inputs.get(Inputs.EMAIL).getText().toString());
-                            userService.putExtra("password", inputs.get(Inputs.PASSWORD).getText().toString());
-
-                            startService(userService);
-
-                            navigateToSignInActivity();
-                            stopService(userService);
+                            db.collection("users")
+                                    .whereEqualTo("email", inputs.get(Inputs.EMAIL).getText().toString())
+                                    .get()
+                                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                            if (task.isSuccessful()) {
+                                                if (task.getResult().getDocuments().isEmpty()) {
+                                                    validatorLock.put(Inputs.EMAIL, true);
+                                                    runUserService();
+                                                } else {
+                                                    validators.get(Inputs.EMAIL).setText(R.string.email_exists);
+                                                    validatorLock.put(Inputs.EMAIL, false);
+                                                }
+                                                Log.i("RESULT", task.getResult().getDocuments().toString());
+                                            } else {
+                                                Log.d("QUERY_ERROR", "Error getting cocuments", task.getException());
+                                            }
+                                        }
+                                    });
                         } else {
                             Toast.makeText(getApplicationContext(), R.string.fill_all_form, Toast.LENGTH_SHORT).show();
                         }
-
                         break;
                     }
                     default:
                         break;
-
                 }
-
-
                 return true;
             }
         });
@@ -192,30 +203,21 @@ public class SignUpActivity extends AppCompatActivity {
         facebook.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-
                 switch (event.getAction()) {
-
                     case MotionEvent.ACTION_DOWN: {
                         v.setBackgroundResource(R.drawable.ic_facebook_ico_clicked);
                         break;
                     }
                     case MotionEvent.ACTION_UP: {
-
                         v.setBackgroundResource(R.drawable.ic_facebook_ico);
-
-
                         break;
                     }
                     default:
                         break;
                 }
-
-
                 return true;
             }
         });
-
-
     }
 
     public Intent createUserServiceIntent() {
@@ -239,6 +241,18 @@ public class SignUpActivity extends AppCompatActivity {
         Intent mainActivity = new Intent(this, MainActivity.class);
         startActivity(mainActivity);
         finish();
+    }
+
+    public void runUserService() {
+        Intent userService = createUserServiceIntent();
+        userService.putExtra("name", inputs.get(Inputs.NAME).getText().toString());
+        userService.putExtra("surname", inputs.get(Inputs.SURNAME).getText().toString());
+        userService.putExtra("email", inputs.get(Inputs.EMAIL).getText().toString());
+        userService.putExtra("password", inputs.get(Inputs.PASSWORD).getText().toString());
+        startService(userService);
+        navigateToSignInActivity();
+        stopService(userService);
+
     }
 
 

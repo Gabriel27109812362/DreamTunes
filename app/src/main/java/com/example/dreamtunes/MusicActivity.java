aca69ee85c288490c16662c85f0b36919877d791
Gabriel27109812362 <gabriel.opolski98362@gmail.com>
2020-01-29 18:27:27 +0100

    Finish work

--- app/src/main/java/com/example/dreamtunes/MusicActivity.java
+++ app/src/main/java/com/example/dreamtunes/MusicActivity.java
@@ -2,23 +2,35 @@ package com.example.dreamtunes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.media.AudioManager;
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
 
 public class MusicActivity extends AppCompatActivity implements View.OnClickListener {
 
 
-    Button play, pause, stop;
+    Button play, pause, stop, yourAccount, allSongs;
     MediaPlayer mediaPlayer;
+    Vibrator v;
     int pauseCurrentPosition;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        yourAccount = (Button) findViewById(R.id.button_your_account);
+        allSongs = (Button) findViewById(R.id.button_all_songs);
+
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         play = (Button) findViewById(R.id.play);
         pause = (Button) findViewById(R.id.pause);
@@ -27,9 +39,10 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
         play.setOnClickListener(this);
         pause.setOnClickListener(this);
         stop.setOnClickListener(this);
-
+        yourAccount.setOnClickListener(this);
 
     }
+
 
     @Override
     public void onClick(View v) {
@@ -39,6 +52,7 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
                 if (mediaPlayer == null) {
                     mediaPlayer = MediaPlayer.create(this, R.raw.song);
                     mediaPlayer.start();
+                    this.v.vibrate(500);
                 } else if (!mediaPlayer.isPlaying()) {
                     mediaPlayer.seekTo(pauseCurrentPosition);
                     mediaPlayer.start();
@@ -49,6 +63,7 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
                 if (mediaPlayer != null) {
                     mediaPlayer.pause();
                     pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                    this.v.vibrate(500);
                 }
                 break;
 
@@ -56,9 +71,24 @@ public class MusicActivity extends AppCompatActivity implements View.OnClickList
                 if (mediaPlayer != null) {
                     mediaPlayer.stop();
                     mediaPlayer = null;
+                    this.v.vibrate(500);
                 }
+                break;
+
+            case R.id.button_your_account:
+                stop.performClick();
+                navigateToYourAcccount();
+                break;
+            default:
                 break;
         }
 
     }
+
+    public void navigateToYourAcccount() {
+        Intent yourAccount = new Intent(this, YourAccountActivity.class);
+        startActivity(yourAccount);
+        finish();
+    }
+
 }
